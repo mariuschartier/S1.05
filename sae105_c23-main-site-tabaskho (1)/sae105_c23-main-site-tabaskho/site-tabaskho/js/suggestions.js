848f70069f56d1878618b6aadf6e11bd7e6c73bb
mariuschartier <124588435+mariuschartier@users.noreply.github.com>
2024-06-18 01:05:47 +0200

    Add files via upload

---
+++ sae105_c23-main-site-tabaskho (1)/sae105_c23-main-site-tabaskho/site-tabaskho/js/suggestions.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @author Mattéo_KERVADEC
+ * @constant suggestions liste de série possible pour la barre de recherche
+ */
+
+const suggestions = [
+  { serie: "The Last of Us", lien: "the-last-of-us.html" },
+  { serie: "Paolo's Adventure", lien: "Paolo.html" },
+  { serie: "Innosang", lien: "https://youtu.be/8ASkQJEALjc?si=UAP4TcmAYIDHrsG8" },
+  { serie: "Dieu des Frites", lien: "DieuDesFrites.html" },
+  { serie: "Walking Dead", lien: "WalkingDead.html" },
+  { serie: "Man vs Bee", lien: "ManVsBee.html" },
+  { serie: "Atlantis", lien: "Atlantis.html" },
+  { serie: "911", lien: "911.html" },
+  { serie: "Dead or Love", lien: "DeadOrLove.html" },
+  { serie: "Genshin Impact", lien: "GenshinImpact.html" },
+  { serie: "Halo", lien: "Halo.html" },
+  { serie: "Under the Dome", lien: "UnderTheDome.html" },
+  { serie: "Hollow Knight", lien: "HollowNight.html" },
+  { serie: "South Park", lien: "SouthPark.html" },
+];
